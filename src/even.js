1c97d1d0e6e8c9b6d5327a37c09a44961f3ca011
Ross Carter <indovinachi99@gmail.com>
2022-12-13 13:09:31 +0300

    create even.js game logic

---
+++ src/even.js
@@ -0,0 +1,27 @@
+import readlineSync from 'readline-sync';
+
+console.log('Welcome to the Brain Games!');
+
+const userName = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${userName}!`);
+
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+const evenNumbers = () => {
+  for (let i = 0; i < 3; i += 1) {
+    const randomNum = Math.floor(Math.random() * 100) + 1;
+    console.log(`Question: ${randomNum} `);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if ((randomNum % 2 === 0 && userAnswer === 'yes') || (randomNum % 2 !== 0 && userAnswer === 'no')) {
+      console.log('Correct!');
+    } else {
+      const oppositeAnswer = userAnswer === 'yes' ? 'no' : 'yes';
+      const gameOver = console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${oppositeAnswer}'.\n Let's try again, ${userName}!`);
+      return gameOver;
+    }
+  }
+  const gameWin = console.log(`Congratulations, ${userName}!`);
+  return gameWin;
+};
+
+export default evenNumbers;
